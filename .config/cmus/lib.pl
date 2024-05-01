26d9efb5af3865181a13171dec2e88334176a842
NyanMaths <nyanmaths@gmail.com>
2024-05-01 16:20:56 +0200

    added base config and user scripts

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,273 @@
+/home/nyanmaths/Music/Albums/enfantillages 3/01 Piste 1.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/02 Piste 2.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/03 Piste 3.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/04 Piste 4.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/05 Piste 5.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/06 Piste 6.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/07 Piste 7.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/08 Piste 8.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/09 Piste 9.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/10 Piste 10.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/11 Piste 11.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/12 Piste 12.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/13 Piste 13.ogg
+/home/nyanmaths/Music/Albums/enfantillages 3/14 Piste 14.ogg
+/home/nyanmaths/Music/Albums/LSD/Audio - LSD.ogg
+/home/nyanmaths/Music/Albums/LSD/Genius - LSD.ogg
+/home/nyanmaths/Music/Albums/LSD/Thunderclouds - LSD.ogg
+/home/nyanmaths/Music/Albums/The Evol/01 Gung Ho.ogg
+/home/nyanmaths/Music/Albums/The Evol/02 Fear Ya.ogg
+/home/nyanmaths/Music/Albums/The Evol/03 Faking Love.ogg
+/home/nyanmaths/Music/Albums/The Evol/04 Bunker.ogg
+/home/nyanmaths/Music/Albums/The Evol/05 On fire.ogg
+/home/nyanmaths/Music/Albums/The Evol/06 Summer Camp.ogg
+/home/nyanmaths/Music/Albums/The Evol/07 Wrong Side.ogg
+/home/nyanmaths/Music/Albums/The Evol/08 Wataman.ogg
+/home/nyanmaths/Music/Albums/The Evol/09 slam ans slam'ed.ogg
+/home/nyanmaths/Music/Albums/The Evol/10 Rusty Fonky.ogg
+/home/nyanmaths/Music/Albums/The Evol/11 Share a line.ogg
+/home/nyanmaths/Music/Albums/The Evol/12 Mysterious Ways.ogg
+/home/nyanmaths/Music/Albums/The Evol/13 Killing Hallelujah.ogg
+/home/nyanmaths/Music/Alestorm/Alestorm - Cannonball.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Death Throes of the Terrorsquid.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Drink.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm -Fannybaws.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Hangover.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Keelhauled.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Magnetic North.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Mexico.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - P.A.R.T.Y..mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Pirate Metal Drinking Crew.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Shipwrecked.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - The Battle of Cartagena.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Treasure Chest Party Quest.mp3
+/home/nyanmaths/Music/Bad Apple - Cosmowave remix.mp3
+/home/nyanmaths/Music/Baiana - Bakermat.ogg
+/home/nyanmaths/Music/Billie Ellish/All the good girls go to hell.ogg
+/home/nyanmaths/Music/Billie Ellish/Bad guy - Billie Eilish.ogg
+/home/nyanmaths/Music/Billie Ellish/Bellyache - Billie Eilish.ogg
+/home/nyanmaths/Music/Billie Ellish/Bury a Friend - Billie Eilish.ogg
+/home/nyanmaths/Music/Billie Ellish/COPYCAT - Billie Eilish.ogg
+/home/nyanmaths/Music/Billie Ellish/idontwannabeyouanymore - Billie Eilish.ogg
+/home/nyanmaths/Music/Billie Ellish/Not Time to Die - Billie Eilish.ogg
+/home/nyanmaths/Music/Billie Ellish/Ocean Eyes - Billie Eilish.ogg
+/home/nyanmaths/Music/Billie Ellish/You should see me in a crown - Billie Eilish.ogg
+/home/nyanmaths/Music/Caramelldansen - Caramella Girls.ogg
+/home/nyanmaths/Music/DJ Honey Bee - Sky Heights.mp3
+/home/nyanmaths/Music/DuoCore - Endorphins.mp3
+/home/nyanmaths/Music/Electro/3rd Prototype - I Know.ogg
+/home/nyanmaths/Music/Electro/Alan Walker - Diamond Heart.ogg
+/home/nyanmaths/Music/Electro/Ascence - About You.ogg
+/home/nyanmaths/Music/Electro/Axol & The Tech Thieves - Bleed.ogg
+/home/nyanmaths/Music/Electro/Clarx & Harddope - Castle.ogg
+/home/nyanmaths/Music/Electro/Cornandbeans - The Traveller.mp3
+/home/nyanmaths/Music/Electro/DRUM GO DUM - KDA.ogg
+/home/nyanmaths/Music/Electro/Earthquake - ROY KNOX.ogg
+/home/nyanmaths/Music/Electro/Ellis - Clear My Head.ogg
+/home/nyanmaths/Music/Electro/Enslaved - Barren Gates & M.I.M.E.ogg
+/home/nyanmaths/Music/Electro/Eternal - Marin Hoxha & Caravn.ogg
+/home/nyanmaths/Music/Electro/f777 - Deadlocked.mp3
+/home/nyanmaths/Music/Electro/Falling For You - Marin Hoxha x Annie Sollange.ogg
+/home/nyanmaths/Music/Electro/Fast Lane - NIVIRO.ogg
+/home/nyanmaths/Music/Electro/Fearless pt.II - TULE.ogg
+/home/nyanmaths/Music/Electro/Fingertips - Kisma.ogg
+/home/nyanmaths/Music/Electro/Flutter - Diamond Eyes.ogg
+/home/nyanmaths/Music/Electro/Forever Bound - Cosmic Harmony.ogg
+/home/nyanmaths/Music/Electro/Half a Heart - BEAUZ.ogg
+/home/nyanmaths/Music/Electro/Happi - Mangoo.ogg
+/home/nyanmaths/Music/Electro/Hoang - Don't Say.ogg
+/home/nyanmaths/Music/Electro/Ignite - K-391 & Alan Walker.ogg
+/home/nyanmaths/Music/Electro/I’ll show you - KDA.mp3
+/home/nyanmaths/Music/Electro/I'm not cool - HyunA.ogg
+/home/nyanmaths/Music/Electro/Jim Yosef - Can't Wait.ogg
+/home/nyanmaths/Music/Electro/K-391 & RØRY - Aurora.ogg
+/home/nyanmaths/Music/Electro/Koven x ROY KNOX - About Me.ogg
+/home/nyanmaths/Music/Electro/Kuyenda - Lennart Schroot & Unknown Brain.ogg
+/home/nyanmaths/Music/Electro/Legends Never Die - LoL & Alan Walker.ogg
+/home/nyanmaths/Music/Electro/Light It Up - Robin Hustin x TobiMorrow.ogg
+/home/nyanmaths/Music/Electro/Lily - Alan Walker.mp3
+/home/nyanmaths/Music/Electro/Live A Lie - Rival x Egzod.ogg
+/home/nyanmaths/Music/Electro/Lost In The Moment - RUBIKA.ogg
+/home/nyanmaths/Music/Electro/Lost Sky - Dreams pt. II.ogg
+/home/nyanmaths/Music/Electro/LukHash - The Other Side.mp3
+/home/nyanmaths/Music/Electro/Lunar Abyss.mp3
+/home/nyanmaths/Music/Electro/Make Me Move - Culture Code.ogg
+/home/nyanmaths/Music/Electro/Más Y Más - Cartoon.ogg
+/home/nyanmaths/Music/Electro/Memories - BEAUZ.ogg
+/home/nyanmaths/Music/Electro/Memories - Sabai.ogg
+/home/nyanmaths/Music/Electro/Million Days - Sabai & Hoang.ogg
+/home/nyanmaths/Music/Electro/Miracle - Kovan & LaCrème.ogg
+/home/nyanmaths/Music/Electro/MORE - KDA.ogg
+/home/nyanmaths/Music/Electro/Mortals - Warriyo.ogg
+/home/nyanmaths/Music/Electro/Nevada - Vicetone.ogg
+/home/nyanmaths/Music/Electro/Never Be Alone - X-Ziliot.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Alive - Agatino Romero.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Better off alone - ZHIKO.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Can you feel it - Ranqz.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Collapse - NYOR.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Creed - Pikasonic.ogg
+/home/nyanmaths/Music/Electro/Nightcore/I kissed a girl - Sunlike Brothers.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Illuminate - KALVYN.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Like a G6 - MEYSTA.ogg
+/home/nyanmaths/Music/Electro/Nightcore/nightcore mix.mp3
+/home/nyanmaths/Music/Electro/Nightcore/Nostalgia - Pikasonic.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Red Line - Anna Yvette.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Throwback - Electro Light.ogg
+/home/nyanmaths/Music/Electro/Nightcore/Wildflowers - Pikasonic.ogg
+/home/nyanmaths/Music/Electro/Nightcore/You are - Dark Point.ogg
+/home/nyanmaths/Music/Electro/Nightlight - Illenium.ogg
+/home/nyanmaths/Music/Electro/Outerspace - BEAUZ.ogg
+/home/nyanmaths/Music/Electro/Outlaw - it's different.ogg
+/home/nyanmaths/Music/Electro/Pain - Ship Wrek.ogg
+/home/nyanmaths/Music/Electro/Phoenix - Cailin Russo & Chrissy Costanza.ogg
+/home/nyanmaths/Music/Electro/Pill - Heuse & Zeus x Crona.ogg
+/home/nyanmaths/Music/Electro/Pop Stars - KDA.mp3
+/home/nyanmaths/Music/Electro/Rameses B - Beside You.ogg
+/home/nyanmaths/Music/Electro/Remember - Levianth & Axol.ogg
+/home/nyanmaths/Music/Electro/Retrograde & Xomu - Valhalla.mp3
+/home/nyanmaths/Music/Electro/Rise Up - TheFatRat.ogg
+/home/nyanmaths/Music/Electro/Rival & Cadmium - Daily.ogg
+/home/nyanmaths/Music/Electro/Rival x Cadmium - Doubt You.ogg
+/home/nyanmaths/Music/Electro/Rootkit - Against the Sun.ogg
+/home/nyanmaths/Music/Electro/Safe&Sound & Sendi Hoxha - DEAF KEV.ogg
+/home/nyanmaths/Music/Electro/Save Me - ROY KNOX x Tim Beeren.ogg
+/home/nyanmaths/Music/Electro/Seasons - Rival x Cadmium.ogg
+/home/nyanmaths/Music/Electro/Shadows - Aero Chord.ogg
+/home/nyanmaths/Music/Electro/Shadows - It's different.ogg
+/home/nyanmaths/Music/Electro/Shadows - ROY KNOX x WTCHOUT.ogg
+/home/nyanmaths/Music/Electro/Shining - ROY KNOX.ogg
+/home/nyanmaths/Music/Electro/Something Just Like This - Anthony Keyrouz & Romy Wave.ogg
+/home/nyanmaths/Music/Electro/Something Real - Rameses B.ogg
+/home/nyanmaths/Music/Electro/Stay With Me - Mendum.ogg
+/home/nyanmaths/Music/Electro/Stay - Yves V & Matthew Hill.ogg
+/home/nyanmaths/Music/Electro/Stronger - Lemon Fight.ogg
+/home/nyanmaths/Music/Electro/Stronger - TheFatRat & Slaydit & Anjulie.ogg
+/home/nyanmaths/Music/Electro/Sun Goes Down - Jim Yosef x ROY KNOX.ogg
+/home/nyanmaths/Music/Electro/THE BADDEST - KDA.ogg
+/home/nyanmaths/Music/Electro/The Calling - TheFatRat.ogg
+/home/nyanmaths/Music/Electro/Theory of Everything - DJ Nate.ogg
+/home/nyanmaths/Music/Electro/Tom Wilson - Be Myself.ogg
+/home/nyanmaths/Music/Electro/Too Late - OmgLoSteve.ogg
+/home/nyanmaths/Music/Electro/Tornado - Trivecta.ogg
+/home/nyanmaths/Music/Electro/Valkyrie - Varien.ogg
+/home/nyanmaths/Music/Electro/VILLAIN - KDA.ogg
+/home/nyanmaths/Music/Electro/Waiting - Vicetone.ogg
+/home/nyanmaths/Music/Electro/Walk Thru Fire - Vicetone.ogg
+/home/nyanmaths/Music/Electro/Wasted - Carda.ogg
+/home/nyanmaths/Music/Electro/Waterflame - Clutterfunk.ogg
+/home/nyanmaths/Music/Electro/Way Back - Vicetone.ogg
+/home/nyanmaths/Music/Electro/Weakness - Prismo.ogg
+/home/nyanmaths/Music/Electro/We All - Nonsens & Ericka Jane.ogg
+/home/nyanmaths/Music/Electro/We Said Goodbye - NORTIN & Skyler Cocco.ogg
+/home/nyanmaths/Music/Electro/We won't be alone - Feint.ogg
+/home/nyanmaths/Music/Electro/When You're Lonely - Hoang & Tasji.ogg
+/home/nyanmaths/Music/Electro/Where We Started - Lost Sky.ogg
+/home/nyanmaths/Music/Electro/Why Do I- - Unknown Brain.ogg
+/home/nyanmaths/Music/Electro/Wicked Ways - Killabyte.ogg
+/home/nyanmaths/Music/Electro/Words - Feint.ogg
+/home/nyanmaths/Music/Electro/Wrap me in Plastic - MOMOLAND.ogg
+/home/nyanmaths/Music/Electro/Xaia & Rain Man & Oly - Breakdown.ogg
+/home/nyanmaths/Music/Electro/Zombie - Albert Vishi.ogg
+/home/nyanmaths/Music/Ghost town - The Specials.ogg
+/home/nyanmaths/Music/Iggy Pop & Goran Bregovic - In The Death Car.mp3
+/home/nyanmaths/Music/il vento d'oro.mp3
+/home/nyanmaths/Music/Inabakumori - Lagtrain.mp3
+/home/nyanmaths/Music/In hell I'll be in good company.ogg
+/home/nyanmaths/Music/Iron Maiden - The Trooper.ogg
+/home/nyanmaths/Music/Is There Anyone Else Covered In Fresh Blood ~ Who Cares - ZUN.mp3
+/home/nyanmaths/Music/Jain/Abu Dhabi - Jain.mp3
+/home/nyanmaths/Music/Jain/Alright - Jain.mp3
+/home/nyanmaths/Music/Jain/Dynabeat - Jain.mp3
+/home/nyanmaths/Music/Jain/Heads up - Jain.mp3
+/home/nyanmaths/Music/Jain/Makeba - Jain.mp3
+/home/nyanmaths/Music/Jain/On my way - Jain.mp3
+/home/nyanmaths/Music/Jain/Son of a Sun - Jain.mp3
+/home/nyanmaths/Music/Jain/Star - Jain.mp3
+/home/nyanmaths/Music/Killing In the Name - RATM.ogg
+/home/nyanmaths/Music/La Main Verte.ogg
+/home/nyanmaths/Music/L'apologie.ogg
+/home/nyanmaths/Music/Lean on - Major Laser.ogg
+/home/nyanmaths/Music/Les Ramoneurs de Menhirs - Menez Daou.ogg
+/home/nyanmaths/Music/Light it Up - Major Lazer.ogg
+/home/nyanmaths/Music/Like boys - Hyphen Hyphen.ogg
+/home/nyanmaths/Music/Loco Contigo - DJ Sanke.ogg
+/home/nyanmaths/Music/Motivés - Le chant des partisans.mp3
+/home/nyanmaths/Music/Night of Nights - ZUN.mp3
+/home/nyanmaths/Music/Our God is an Awesome God.mp3
+/home/nyanmaths/Music/Physical - Dua Lipa.ogg
+/home/nyanmaths/Music/Que Calor - Major Laser.ogg
+/home/nyanmaths/Music/Recordings/Diamond Heart - Sofia Somajo & Alan Walker.ogg
+/home/nyanmaths/Music/Recordings/Falling for you record.flac
+/home/nyanmaths/Music/Recordings/Ocean Eyes - Billie Eilish.flac
+/home/nyanmaths/Music/Recordings/sandbox/Fri Feb 25 15.24.11 2022.flac
+/home/nyanmaths/Music/Recordings/sandbox/Fri Feb 25 15.32.03 2022.flac
+/home/nyanmaths/Music/Recordings/sandbox/Tue Mar 1 21.00.23 2022.flac
+/home/nyanmaths/Music/Recordings/Smile remix.flac
+/home/nyanmaths/Music/Recordings/Theory of Everything - DJ Nate.ogg
+/home/nyanmaths/Music/Recordings/xd.flac
+/home/nyanmaths/Music/Rules of Tekken.mp3
+/home/nyanmaths/Music/Smells like teen spirit.ogg
+/home/nyanmaths/Music/Taki Taki.ogg
+/home/nyanmaths/Music/Therapie TAXI/Adena - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Avec ta zouz - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/BB la nuit - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Brutal Summer - Therapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Candide Crush - Therapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Chula - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Disco Lady - Therapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Dominer le bruit - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Egotrip - Therapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Jean Paul - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/J'en ai marre - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/L'équilibre - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Madame Klaude - Therapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Naïve - Therapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Noir - Therapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Pigalle - Therapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/PVP - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Tous les autres - Therapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Voyage - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/YX - Therapie TAXI.ogg
+/home/nyanmaths/Music/Tombé du ciel.ogg
+/home/nyanmaths/Music/Electro/Jet Set.mp3
+/home/nyanmaths/Music/Electro/Zipangu.mp3
+/home/nyanmaths/Music/Electro/Hypermania.mp3
+/home/nyanmaths/Music/Jacques Melissa - Fray.mp3
+/home/nyanmaths/Music/Electro/Slopelocked - SB.mp3
+/home/nyanmaths/Music/Billie Ellish/Lovely - Billie Eilish, Khalid.ogg
+/home/nyanmaths/Music/Billie Ellish/My Boy - Billie Eilish.ogg
+/home/nyanmaths/Music/Billie Ellish/Watch - Billie Eilish.ogg
+/home/nyanmaths/Music/Electro/ColBreakZ - Gold Ring.mp3
+/home/nyanmaths/Music/Cornandbeans - Blaze.mp3
+/home/nyanmaths/Music/Alestorm/Alestorm - Alestorm.m4a
+/home/nyanmaths/Music/Féloche - Darwin avait raison.ogg
+/home/nyanmaths/Music/La fille du coupeur de joints.ogg
+/home/nyanmaths/Music/Jahneration - Reload.ogg
+/home/nyanmaths/Music/Le retour à la terre.ogg
+/home/nyanmaths/Music/Nous y sommes.ogg
+/home/nyanmaths/Music/Therapie TAXI/Hit sale - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Pierpoljak [Radio Edit].ogg
+/home/nyanmaths/Music/Demons Are a Girl's Best Friend - PowerWolf.m4a
+/home/nyanmaths/Music/I'm picky.ogg
+/home/nyanmaths/Music/Wanna Get Free.ogg
+/home/nyanmaths/Music/Story O' My LF.ogg
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/01 Hit Sale (feat. Roméo Elvis).mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/02 J'en ai marre.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/03 Cadence.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/04 Marlboro Bled.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/05 Coma idyllique.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/06 PVP.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/07 Transatlantique.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/08 Salop(e).mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/09 La proue.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/10 Superstar.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/11 Crystal Memphis.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/12 Cri des loups.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/13 Zarba.mp3
+/home/nyanmaths/Music/Albums/Therapie TAXI - Hit Sale/14 Anti Hit Sale.mp3
+/home/nyanmaths/Music/Therapie TAXI/Cadence - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Crystal Memphis - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Therapie TAXI/Salope - Thérapie TAXI.ogg
+/home/nyanmaths/Music/Tryo - Desole pour hier soir.ogg
+/home/nyanmaths/Music/Electro/Turbo - Säkkijärven Polkka.m4a
+/home/nyanmaths/Music/Therapie TAXI/Parallèle - Thérapie TAXI.ogg
---
+++ .config/hypr/scripts/touchpad-toggle.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
+
+enable_touchpad() {
+    printf "true" >"$STATUS_FILE"
+    notify-send -u normal "enabling touchpad"
+    hyprctl keyword '$TOUCHPAD_ENABLED' "true" -r
+}
+
+disable_touchpad() {
+    printf "false" >"$STATUS_FILE"
+    notify-send -u normal "disabling touchpad"
+    hyprctl keyword '$TOUCHPAD_ENABLED' "false" -r
+}
+
+if ! [ -f "$STATUS_FILE" ]; then
+  enable_touchpad
+else
+  if [ $(cat "$STATUS_FILE") = "true" ]; then
+    disable_touchpad
+  elif [ $(cat "$STATUS_FILE") = "false" ]; then
+    enable_touchpad
+  fi
+fi
